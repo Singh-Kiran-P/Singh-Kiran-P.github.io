6336a43791c6c6beb6ceb61661b4bb9038169f5c
Kiran Pal Singh <singh.kiran2456@gmail.com>
2025-03-29 10:18:15 +0000

    update

--- lib/actions.ts
+++ lib/actions.ts
@@ -135,7 +135,7 @@ export async function sendContactForm({ name, email, message }: ContactFormProps
       
       const { data1, error1 } = await resend.emails.send({
         from: "Kscode <info@kscode.eu>",
-        to: "singh.kiran2456@gmail.com",
+        to: "kiran@kscode.eu",
         subject: "New Contact Form Submission",
         html: `
           <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f9f9f9; border-radius: 8px; box-shadow: 0 2px 10px rgba(0,0,0,0.1);">
